4a600c23cf318c463ef6822fb4d63cd9841356e1
anjehle <anjehle@umich.edu>
2021-11-07 11:10:43 -0800

    Creates ModelAdmin class for categories

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts')
+
+
 admin.site.register(Post)
 admin.site.register(Category)
