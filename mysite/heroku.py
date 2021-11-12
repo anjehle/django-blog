6b989d38fe28695292ee3d085a9446226a921a6b
anjehle <anjehle@umich.edu>
2021-11-11 23:19:51 -0800

    Adds GitHub Auth

--- mysite/heroku.py
+++ mysite/heroku.py
@@ -20,3 +20,15 @@ MIDDLEWARE = (
     "whitenoise.middleware.WhiteNoiseMiddleware",
     *MIDDLEWARE,
 )
+
+# config/settings.py
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+LOGIN_REDIRECT_URL = 'home'
---
+++ views.py
@@ -0,0 +1,5 @@
+# config/views.py
+from django.views.generic import TemplateView
+
+class Home(TemplateView):
+    template_name = 'home.html'
