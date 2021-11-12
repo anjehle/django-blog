be7efb101037529722949a2a3d8e81eb9cdee10b
anjehle <anjehle@umich.edu>
2021-11-11 23:24:45 -0800

    Fixes url path

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('blogging.urls')),
+    # path('', include('blogging.urls')),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
     path('', Home.as_view(), name='home'),
